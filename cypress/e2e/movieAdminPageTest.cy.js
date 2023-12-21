5e4f51d898cae8fc98a99de7ba86df9257ac92be
Artem <s-art7@bk.ru>
2023-12-21 16:01:29 +0200

    Initial commit

---
+++ cypress/e2e/movieAdminPageTest.cy.js
@@ -0,0 +1,43 @@
+import admin from "../fixtures/admin.json";
+import sel from "../fixtures/selectors.json";
+
+describe("Movie administration page test", () => {
+  beforeEach(() => {
+    cy.fixture("admin.json").as("data");
+    cy.visit("/admin/");
+  });
+
+  it("Login page correct display test", () => {
+    cy.get(sel.adminText).should("be.visible");
+  });
+
+  it("Should log in", () => {
+    cy.login(admin[0].email, admin[0].password);
+    cy.get(sel.hallsControl).should("be.visible");
+  });
+
+  it("Should throw an error if email or password incorrect", () => {
+    cy.login(admin[1].email, admin[1].password);
+    cy.get("body").should("contain", "Ошибка авторизации");
+  });
+
+  it("Should book seats", () => {
+    cy.login(admin[0].email, admin[0].password);
+    cy.get(sel.zver)
+      .invoke("text")
+      .then((text) => {
+        console.log(text);
+        cy.visit("https://qamid.tmweb.ru");
+        cy.get(sel.fr).click();
+        cy.get(sel.firstMovieTitle).should("have.text", text);
+      });
+    cy.get(sel.seanceTime).contains("00:00").click();
+    const seats = require("../fixtures/seats.json");
+    seats.forEach((seat) => {
+      cy.get(
+        `.buying-scheme__wrapper > :nth-child(${seat.row}) > :nth-child(${seat.seat})`
+      ).click();
+    });
+    cy.get(sel.accept).should("have.prop", "disabled", false);
+  });
+});
---
+++ cypress/e2e/moviePageTest.cy.js
@@ -0,0 +1,15 @@
+import sel from "../fixtures/selectors.json";
+
+describe("Movie page correct display", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("Should open page", () => {
+    cy.get(sel.title).should("be.visible");
+  });
+
+  it("Should show correct number of days", () => {
+    cy.get(sel.days).should("have.length", 7);
+  });
+});
